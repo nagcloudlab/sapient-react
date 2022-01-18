3386d383a439736a5c4564d1836b471f448b4d99
nagabhushanam <nagabhushanamn@gmail.com>
2022-01-18 11:02:56 +0530

    2 components react demo

--- 3-react-101/intro/src/App.js
+++ 3-react-101/intro/src/App.js
@@ -1,45 +1,41 @@
 
 import React, { Component } from 'react'
+import Greet from './Greet';
 
-/**
- * author : Nag
- * 
- */
+
+// stateful
 class App extends Component {
 
   state = {
     message: "greetings"
   }
-
   constructor(props) {
     super()
     console.log("App::constructor()");
   }
-
   handleEvent(e, message) {
     this.setState({ message })
   }
-
   render() {
     console.log("App::render()");
     let { title, trainer } = this.props
     let { message } = this.state
     return (
       <div className='container'>
-        <hr />
-        <h1 className='text text-danger'>
-          {title} - &nbsp;
-          <small className='h6'>by {trainer}</small>
-        </h1>
-        <hr />
-        <div className='d-flex justify-content-around'>
-          <button onClick={e => this.handleEvent(e, "good morning")} className='btn btn-dark'>GM</button>
-          <button onClick={e => this.handleEvent(e, "good noon")} className='btn btn-dark'>GN</button>
-          <button onClick={e => this.handleEvent(e, "good evening")} className='btn btn-dark'>GE</button>
-        </div>
-        <hr />
-        <div className='alert alert-info'>
-          {message}
+        <div className='card card-body'>
+          <hr />
+          <h1 className='text text-danger'>
+            {title} - &nbsp;
+            <small className='h6'>by {trainer}</small>
+          </h1>
+          <hr />
+          <div className='d-flex justify-content-around'>
+            <button onClick={e => this.handleEvent(e, "good morning")} className='btn btn-dark'>GM</button>
+            <button onClick={e => this.handleEvent(e, "good noon")} className='btn btn-dark'>GN</button>
+            <button onClick={e => this.handleEvent(e, "good evening")} className='btn btn-dark'>GE</button>
+          </div>
+          <hr />
+          <Greet message={message} />
         </div>
       </div>
     )
@@ -49,3 +45,4 @@ class App extends Component {
 
 
 export default App;
+

0782b2f64c7761f278133470e4a4e7b0f5247f45
nagabhushanam <nagabhushanamn@gmail.com>
2022-01-21 14:36:26 +0530

    fix

--- eshop-v1/src/App.js
+++ eshop-v1/src/App.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import classNames from "classnames";
 
 function App() {
 
@@ -8,8 +9,8 @@ function App() {
             name: 'Laptop',
             price: 250000,
             description: "New pro",
-            isAvailable:true,
-            imagePath:'images/Laptop.png'
+            isAvailable: true,
+            imagePath: 'images/Laptop.png'
         },
         {
             id: 2,
@@ -21,21 +22,73 @@ function App() {
         }
     ])
 
-    const renderProducts=()=>{
-        return products.map(product=>{
+    const [tab, setTab] = useState(1)
+
+    const renderTabPanel = product => {
+        switch (tab) {
+            case 1: {
+                return (
+                    <div>{product.description}</div>
+                )
+            }
+            case 2: {
+                return (
+                    <div>Not Yet</div>
+                )
+            }
+            case 3: {
+                return (
+                    <div>None Yet</div>
+                )
+            }
+        }
+    }
+
+    const handleTabChange=(e,tabIndex)=>{
+        e.preventDefault()
+        setTab(tabIndex)
+    }
+
+    const renderProducts = () => {
+        return products.map(product => {
             return (
                 <div key={product.id} className={"list-group-item"}>
-                   <div className={"row"}>
-                       <div className={"col-4"}>
-                           <img className={"img-fluid"} src={product.imagePath}/>
-                       </div>
-                       <div className={"col-8"}>
-                           <div>{product.name}</div>
-                           <div>&#8377;{product.price}.00</div>
-                           <div>{product.description}</div>
-                           <button disabled={!product.isAvailable} className="btn btn-sm btn-dark">buy</button>
-                       </div>
-                   </div>
+                    <div className={"row"}>
+                        <div className={"col-4"}>
+                            <img className={"img-fluid"} src={product.imagePath}/>
+                        </div>
+                        <div className={"col-8"}>
+                            <div>{product.name}</div>
+                            <div>&#8377;{product.price}.00</div>
+                            <button disabled={!product.isAvailable} className="btn btn-sm btn-dark">buy</button>
+                            <br/>
+                            <ul className="nav nav-tabs">
+                                <li className="nav-item">
+                                    <a
+                                        onClick={e => handleTabChange(e,1)}
+                                        className={classNames('nav-link',{active:tab===1})}
+                                        href={""}>
+                                        Description
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a onClick={e => handleTabChange(e,2)}
+                                       className={classNames('nav-link',{active:tab===2})}
+                                       href={""}>
+                                        Specifications
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a onClick={e => handleTabChange(e,3)}
+                                       className={classNames('nav-link',{active:tab===3})}
+                                       href={""}>
+                                        Reviews
+                                    </a>
+                                </li>
+                            </ul>
+                            {renderTabPanel(product)}
+                        </div>
+                    </div>
                 </div>
             )
         })
@@ -55,6 +108,8 @@ function App() {
             </div>
         </div>
     );
+
+    
 }
 
 export default App;
